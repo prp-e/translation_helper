49c5e8ff616cdca4464b0f3e893d1cd4e051ca5c
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-12-08 12:34:22 +0330

    updated: routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   resources :projects do 
     resources :properties
   end 
-  
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'projects#index'
+  get 'projects/:project_id/json' => 'json_out#index'
 end
