a8c3386b3f5e238bdbfe2a09003a3ef29b7eec9c
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-12-08 11:35:00 +0330

    Added the project scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_08_080317) do
+ActiveRecord::Schema.define(version: 2021_12_08_080443) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
