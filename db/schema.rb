28665dbdc0c4b10e920e9aa34b2f76cab6c4c313
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-12-08 11:43:28 +0330

    updated: database schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_08_080443) do
+ActiveRecord::Schema.define(version: 2021_12_08_081122) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "properties", force: :cascade do |t|
+    t.integer "project_id", null: false
+    t.string "label"
+    t.text "en"
+    t.text "fa"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["project_id"], name: "index_properties_on_project_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +40,5 @@ ActiveRecord::Schema.define(version: 2021_12_08_080443) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "properties", "projects"
 end
