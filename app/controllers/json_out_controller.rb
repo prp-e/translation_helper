17f8aabbd3da73fd0e647f98173e38fb7dfada55
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-12-08 12:32:23 +0330

    created a simple index for json_out controller

--- app/controllers/json_out_controller.rb
+++ app/controllers/json_out_controller.rb
@@ -1,2 +1,12 @@
 class JsonOutController < ApplicationController
+    before_action :find_project 
+
+    def index
+        render json: @project.to_json
+    end 
+
+    private
+    def find_project
+        @project = Project.find(params[:project_id])
+    end
 end
