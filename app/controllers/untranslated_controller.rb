a4a3fcf9417a089019b93966a3bdfef14d5876cd
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-12-10 18:09:41 +0330

    made the untranslated controller, tests remaining

--- app/controllers/untranslated_controller.rb
+++ app/controllers/untranslated_controller.rb
@@ -1,4 +1,12 @@
 class UntranslatedController < ApplicationController
+  before_action :authenticate_user!
+  before_action :get_project
   def index
+    @properties = @project.properties.where(en: "")
+  end
+
+  private
+  def get_project
+    @project = Project.find(params[:project_id])
   end
 end
