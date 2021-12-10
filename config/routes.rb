c292c3a3aa2e5cc42c92c438aa0bfdbe8becb116
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-12-10 18:11:17 +0330

    updated: routing

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'projects#index'
   get 'projects/:project_id/json' => 'json_out#index'
+  get 'projects/:project_id/untranslated' => 'untranslated#index'
 end
